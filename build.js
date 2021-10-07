a759f992252cdaa95d11bfa5b599a698e193483d
arpitdalal <arpitdalal@gmail.com>
2021-10-07 15:16:48 -0400

    fix #6

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
-import s from 'shelljs';
-import config from './tsconfig.json';
+const s = require('shelljs');
+const config = require('./tsconfig.json');
 const outDir = config.compilerOptions.outDir;
 
 s.rm('-rf', outDir);
