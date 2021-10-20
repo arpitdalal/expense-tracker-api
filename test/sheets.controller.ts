86af269d82e6f45be109b850d346445d5d7a7e5d
arpitdalal <arpitdalal@gmail.com>
2021-10-20 09:19:31 -0400

    update tests to comply with the new api spec

--- test/sheets.controller.ts
+++ test/sheets.controller.ts
@@ -1,50 +1,50 @@
-import "mocha";
-import request from "supertest";
+import 'mocha';
+import request from 'supertest';
 
-import Server from "../server";
+import Server from '../server';
 
-const sheetId = "120LwrLFrFqafIWEFno2hGUBFn-y7mGIL0A2MADfMo48";
+const sheetId = '120LwrLFrFqafIWEFno2hGUBFn-y7mGIL0A2MADfMo48';
 
-describe("Sheets", () => {
-  it("should insert a row", (done) => {
+describe('Sheets', () => {
+  it('should insert a row', (done) => {
     request(Server)
       .post(`/api/v1/sheets/${sheetId}`)
-      .set("Accept", "application/json")
-      .send({ title: "Test", expense: 40 })
-      .expect("Content-Type", /json/)
+      .set('Accept', 'application/json')
+      .send({ title: 'Test', expense: '40' })
+      .expect('Content-Type', /json/)
       .expect(201)
       .then(() => done())
       .catch(done);
   });
-  it("should update a row with title and expense", (done) => {
+  it('should update a row with title and expense', (done) => {
     request(Server)
       .patch(`/api/v1/sheets/${sheetId}`)
-      .send({ id: 0, title: "test1", expense: -40 })
+      .send({ id: 0, title: 'test1', expense: '-40' })
       .expect(200)
       .then(() => done())
       .catch(done);
   });
-  it("should update a row with only title", (done) => {
+  it('should update a row with only title', (done) => {
     request(Server)
       .patch(`/api/v1/sheets/${sheetId}`)
-      .send({ id: 0, title: "test2" })
+      .send({ id: 0, title: 'test2' })
       .expect(200)
       .then(() => done())
       .catch(done);
   });
-  it("should update a row with only expense", (done) => {
+  it('should update a row with only expense', (done) => {
     request(Server)
       .patch(`/api/v1/sheets/${sheetId}`)
-      .send({ id: 0, expense: 10 })
+      .send({ id: 0, expense: '10' })
       .expect(200)
       .then(() => done())
       .catch(done);
   });
-  it("should delete a row", (done) => {
+  it('should delete a row', (done) => {
     request(Server)
       .delete(`/api/v1/sheets/${sheetId}`)
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
       .send({ id: 0 })
       .expect(200)
       .then(() => done())
