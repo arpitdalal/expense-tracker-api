e426739d1768e2d4cc04843b8ad011f2b849600c
arpitdalal <arpitdalal@gmail.com>
2021-10-20 12:33:57 -0400

    fix the PEM routines issue with https://github.com/auth0/node-jsonwebtoken/issues/642#issuecomment-585173594

--- server/api/utils/getSheetDoc.ts
+++ server/api/utils/getSheetDoc.ts
@@ -9,7 +9,10 @@ export const getSheetDoc = async (
   // Initialize Auth
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL || '',
-    private_key: process.env.GOOGLE_PRIVATE_KEY || '',
+    private_key:
+      (process.env.GOOGLE_PRIVATE_KEY &&
+        process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n')) ||
+      '',
   });
 
   // Load the document
