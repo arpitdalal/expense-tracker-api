a11d5a37790fc4da8eda8810b7e19fd71ec80f27
arpitdalal <arpitdalal@gmail.com>
2021-11-01 17:22:53 -0400

    fix #33

--- build.ts
+++ build.ts
@@ -5,4 +5,6 @@ const outDir = config.compilerOptions.outDir;
 s.rm('-rf', outDir);
 s.mkdir(outDir);
 s.mkdir('-p', `${outDir}/common/swagger`);
-s.cp('server/common/api.yml', `${outDir}/common/api.yml`);
+s.cp('server/common/swagger-config.yml', `${outDir}/common/swagger-config.yml`);
+s.cp('server/common/v1.api.yml', `${outDir}/common/v1.api.yml`);
+s.cp('server/common/v2.api.yml', `${outDir}/common/v2.api.yml`);
